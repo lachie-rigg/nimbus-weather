697e966063aa08a24c4c3c6294f01b17557a80bb
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-12-08 20:01:08 +1100

    Initial logo complete, placeholder implemented and custom floating navbar started

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NimbusLogo from '../assets/Nimbus.png' 
 
 const Navbar: React.FC = () => {
   return (
     <div className="flex m-2 justify-around items-center h-20 bg-gray-800 text-white rounded-4xl">
+      <img src={NimbusLogo} alt="Nimbus Logo" className="h-24 w-24 mr-4"/>
       <h1 className="text-2xl font-bold">Nimbus Weather</h1>
     </div>
   )
