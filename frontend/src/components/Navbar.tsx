1f95253ae31a03d6406040ef8bef92f7f3908fc8
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-12-08 17:35:16 +1100

    Inital setup for frontend

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex m-2 justify-around items-center h-20 bg-gray-800 text-white rounded-4xl">
+      <h1 className="text-2xl font-bold">Nimbus Weather</h1>
+    </div>
+  )
+}
+export default Navbar;
