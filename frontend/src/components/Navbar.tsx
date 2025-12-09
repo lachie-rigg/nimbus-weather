d05a6ed1db7c62ec79e11a811804871d23fbedd7
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-12-10 05:26:02 +1100

    inital design setup for navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import NimbusLogo from '../assets/Nimbus.png' 
+import SearchBar from './SearchBar';
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex m-2 justify-around items-center h-20 bg-gray-800 text-white rounded-4xl">
-      <img src={NimbusLogo} alt="Nimbus Logo" className="h-24 w-24 mr-4"/>
-      <h1 className="text-2xl font-bold">Nimbus Weather</h1>
+    <div className="flex m-4 justify-around items-center h-18 bg-[#4175c0] text-white rounded-xl">
+      <div className='w-1/2 h-full flex justify-start items-center'>
+        <img src={NimbusLogo} alt="Nimbus Logo" className="h-24 w-24 mr-4"/>
+        <h1 className="text-2xl font-extralight">Nimbus Weather</h1>
+      </div>
+      <SearchBar/>
     </div>
   )
 }
---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar: React.FC = () => {
+return (
+  <input className='h-8 p-4 w-1/3 text-black rounded-4xl bg-white' placeholder='Search' type='search'/>
+)    
+}
+
+export default SearchBar;
