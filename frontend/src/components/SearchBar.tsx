6408775ce7d84eb5fff1db11348094213d32c36f
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2026-01-05 08:35:31 +1100

    Inital setup for search bar functionality

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const SearchBar: React.FC = () => {
+  const [query, setQuery] = useState(""); 
+  const [results, setResults] = useState(""); 
+
+  // Updates suburb options based on the users entry in search
+  useEffect(() => {
+    if (query.length < 2) return; 
+    
+    const timer = setTimeout(async () => {
+      const res = await fetch(`https://australiansuburbs.au/api/lookup_suburb?search=${query}`);
+      const data = await res.json();
+      setResults(data); 
+      console.log(results); 
+    }, 500);
+
+    return () => clearTimeout(timer); 
+  }, [query]); 
+
 return (
-  <input className='h-8 p-4 w-1/3 text-black rounded-4xl bg-white' placeholder='Search' type='search'/>
+<div className="relative w-1/3">
+  <span className="pointer-events-none absolute left-4 top-1/2 -translate-y-1/2 text-gray-400">
+    🔎
+  </span>
+  <input
+    type="search"
+    autoCapitalize='words'
+    placeholder="Search your Address, City or Postcode..."
+    className="h-8 w-full rounded-full bg-white pl-10 pr-4 text-black focus:outline-none"
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+  />
+</div>
 )    
 }
 
